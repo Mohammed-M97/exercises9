5f39d62a43422800f2484000805c442e220e26c4
Mohammed-M97 <mohammed-alawlaqi@outlook.com>
2024-07-19 12:26:00 +0300

    add all changes

---
+++ Ex1.js
@@ -0,0 +1,7 @@
+// My head is at the wrong end!
+function fixTheMeerkat(arr) {
+  //return [arr[2], arr[1], arr[0]]
+  return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["bottom", "middle", "top"]));
